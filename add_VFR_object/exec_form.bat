a13505a0d1dc6c83eb8e58d8e462ca62c7235a7d
limagolf22 <70839782+limagolf22@users.noreply.github.com>
2021-03-16 23:54:41 +0100

    Update exec_form.bat
    now move all the folder created by compilation

--- add_VFR_object/exec_form.bat
+++ add_VFR_object/exec_form.bat
@@ -3,5 +3,5 @@ cd ./scripts & script_form.exe %*
 cd ../fspackagetool & fspackagetool.exe ../VFR_object_package/Project-add-VFR-object.xml -outputdir ../VFR_object_package
 cd ../
 cd ../
-copy %CD%\add_VFR_object\VFR_object_package\Packages\add-VFR-object\scenery\VFR_object.bgl %CD%\VFR_object_scenery\scenery\
+xcopy %CD%\add_VFR_object\VFR_object_package\Packages\add-VFR-object %CD%\VFR_object_scenery /e /y /i
 exit
